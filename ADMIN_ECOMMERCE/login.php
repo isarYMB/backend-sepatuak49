3f03e588e75a3e35a366ddfc91defa92783a6682
isarYMB <yizchard0897@gmail.com>
2022-11-04 01:44:10 +0800

    Ready v1.0

--- ADMIN_ECOMMERCE/login.php
+++ ADMIN_ECOMMERCE/login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 // $idStandart = isset($_GET['standar']) ? $_GET['standar'] : 0;
 // $idSubStandart1 = isset($_GET['sub_standar1']) ? $_GET['sub_standar1'] : 0;
@@ -6,112 +6,115 @@ session_start();
 require 'dbconnect.php';
 
 if (isset($_POST['submit'])) {
-  
+
   $user = $_POST['username'];
   $password = $_POST['password'];
 
   $result = mysqli_query($conn, "SELECT * FROM USERS WHERE EMAIL = '$user'");
 
   if (mysqli_num_rows($result) === 1) {
-      $row = mysqli_fetch_assoc($result);
-    
-      if ($password === $row['PASS']) {
-        $level = $row['LEVEL'];
-        if ($level == 0){
-          $_SESSION['login'] = true;
-          $_SESSION['username'] = $row['EMAIL'];
-          $_SESSION['timer'] = time();
-          header('Location: index.php');
-          exit;
-        }else {
-          echo "<script>
-              alert('Anda bukan admin');
-            </script>" ;
-        }
-      }else {
+    $row = mysqli_fetch_assoc($result);
+
+    if (password_verify($password, $row['PASS'])) {
+      $level = $row['LEVEL'];
+      if ($level == 0) {
+        $_SESSION['login'] = true;
+        $_SESSION['username'] = $row['EMAIL'];
+        $_SESSION['timer'] = time();
+        header('Location: index.php');
+        exit;
+      } else {
         echo "<script>
-              alert('Password salah');
-            </script>" ;
+              alert('Anda bukan admin');
+            </script>";
       }
-  }else {
+    } else {
+      echo "<script>
+              alert('Password salah');
+            </script>";
+    }
+  } else {
     echo "<script>
               alert('User tidak di temukan');
-          </script>" ;
+          </script>";
   }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AK49 LOGIN</title>
 
- <!-- Google Font: Source Sans Pro -->
- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
- <!-- Font Awesome -->
- <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
- <!-- Ionicons -->
- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
- <!-- Tempusdominus Bootstrap 4 -->
- <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
- <!-- iCheck -->
- <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
- <!-- Theme style -->
- <link rel="stylesheet" href="dist/css/adminlte.min.css">  
- <!-- overlayScrollbars -->
- <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
- <!-- Daterange picker -->
- <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
- <!-- Hover link -->
- <link rel="stylesheet" href="dist/css/text-link.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- Hover link -->
+  <link rel="stylesheet" href="dist/css/text-link.css">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="#" class="h1"><b>AK</b>49</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Log in admin AK49 </p>
 
-      <form action="" method="post" id="form" name="login">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Username" name="username" id="username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password" id="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="#" class="h1"><b>AK</b>49</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Log in admin AK49 </p>
+
+        <form action="" method="post" id="form" name="login">
           <div class="input-group mb-3">
-            <button type="submit" name="submit" class="btn btn-primary btn-block" >Log In</button>
+            <input type="text" class="form-control" placeholder="Username" name="username" id="username">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <button type="submit" name="submit" class="btn btn-primary btn-block">Log In</button>
           </div>
           <!-- /.col -->
-        </div>
+      </div>
       </form>
     </div>
     <!-- /.card-body -->
   </div>
   <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  </div>
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
